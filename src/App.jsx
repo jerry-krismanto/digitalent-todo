0b3eafc3af6e20c8d788b361bc5d89018d452ef2
Jerry Yan Krismanto <jerryykrismanto@gmail.com>
2024-06-04 19:22:22 +0700

    added tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-// import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
+  const todos = [
+    {
+      id: 1,
+      title: "Learn React",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Learn Vue",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Learn Angular",
+      completed: false,
+    },
+  ];
 
   return (
     <>
-      <h1>To do app</h1>
+      <h1 className="text-4xl font-bold m-12 text-center">To do app</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
